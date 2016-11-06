59561ad8edf0596c215341f0c88ff81c3a292cb7
Navin Udiaver <navinudiaver@navins-mbp.attlocal.net>
2016-11-06 16:09:58 -0600

    outlined js tasks

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// Add interactivity so the user can manage daily tasks
+
+
+// Add a new task
+	// Create a task when button is pressed (list idtm)
+	// New list item with:
+	 	//check box 
+	 	//label 
+	 	//text input 
+	 	//edit button 
+	 	//delete button
+	 	//each element needs to be modified and appended
+
+// Edit an existing task
+	// When edit button is pressed
+		// if the class of parent is .editMode
+			// Switch from .editMode
+			// label text becomes the input's value
+		// else
+			// Switch to .editMode
+			// Input value becomes label text
+
+		// Toggle .editMode on the parent
+
+// Delete an existing task
+	// When delete button is pressed
+		// Remove parent list item from ul
+
+// Mark a task as complete
+	// When the checkbox is checked
+		// Append the task list item to the #completed-tasks
+
+// Mark a task as incomplete
+	// When the checkbox is unchecked
+		// Append the task list item to the #incomplete-tasks
